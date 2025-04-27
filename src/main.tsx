d0c8d6f7d3b38dc57510c9b875cf194ed1ad82d7
admodev <admodevcodes@outlook.com>
2025-04-27 13:27:55 -0300

    refactor: :bug: add 404 page and error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,19 @@ import './index.css';
 // Views
 import Home from './pages/home/Home';
 import Projects from './pages/projects/Projects';
+import NotFoundPage from './shared/views/NotFoundPage';
+
+// Components
+import ErrorBoundary from './shared/components/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/projects' element={<Projects />} />
-    </Routes>
-  </BrowserRouter>
+  <ErrorBoundary>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </ErrorBoundary>
 );
---
+++ src/shared/components/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+// src/components/ErrorBoundary.tsx
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+          <p>Please refresh the page or try again later.</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
