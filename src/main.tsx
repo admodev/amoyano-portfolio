332094337f3bfe6170ac7e42a3527c8a4fc33a82
admodev <admodevcodes@outlook.com>
2025-04-27 13:10:38 -0300

    feat: :sparkles: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// React modules
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Third party modules
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+// Styles
+import './index.css';
+
+// Views
+import Home from './pages/home/Home';
+import Projects from './pages/projects/Projects';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/projects' element={<Projects />} />
+    </Routes>
+  </BrowserRouter>
+);
