0ca824b2f17bb42d29c0b2a4b4d69706e4cf1642
admodev <admodevcodes@outlook.com>
2025-05-01 14:26:59 -0300

    feat: :lipstick: add main title, subtitle and image
    feat: :lipstick: add main title sub and image

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,14 +3,26 @@ import React from 'react';
 // Styles
 import styles from './home.module.css';
 
+// Assets
+import profilepic from '../../assets/profilepic.jpeg';
+
 const Home: React.FC = () => {
   return (
-    <>
+    <div className={styles.mainHeaderContainer}>
       <div className={styles.heroHeaderContainer}>
-        <h2 className={styles.heroHeaderContainerTitle}>Adolfo Moyano</h2>
+        <h2 className={styles.heroHeaderContainerTitle}>Full Stack Engineer | Cloud Specialist</h2>
         <h4 className={styles.heroHeaderContainerSubtitle}>Available for Work</h4>
       </div>
-    </>
+      <div>
+        <h2 className={styles.title}>Hello, i&apos;m Adolfo Moyano</h2>
+        <p className={styles.subtitle}>
+          Full stack engineer, devops and cloud <br /> specialist from Buenos Aires, Argentina.
+        </p>
+        <div>
+          <img src={profilepic} alt='profilepic' />
+        </div>
+      </div>
+    </div>
   );
 };
 
